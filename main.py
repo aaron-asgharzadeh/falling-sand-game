52fa13714e02ddefdeb2596ec0d4919f058e388a
Aaron <aaron.asgharzadeh@gmail.com>
2026-05-29 21:20:11 +0200

    feat: Create simple game loop

--- main.py
+++ main.py
@@ -5,23 +5,34 @@ pygame.init()
 size = width, height = 1377, 768
 speed = [1, 1]
 black = 0, 0, 0
+player_x = 0
+player_y = 0
+cam_x = 0
+cam_y = 0
 
 screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
 
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+player = pygame.image.load("intro_ball.gif").convert()
+
+background = pygame.image.load("background.jpeg").convert()
+screen.blit(background, (-cam_x, -cam_y))
+print(background.get_size())
+
+screen.blit(player, (player_x, player_y))
+
+
+pygame.display.update()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+    # cam_x += 1
+    # player_x += 2
+    # screen.blit(background, (-cam_x, -cam_y))
 
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+    # screen.blit(player, (player_x - cam_x, player_y - cam_y))
+    pygame.display.update()
+    clock.tick(60)
